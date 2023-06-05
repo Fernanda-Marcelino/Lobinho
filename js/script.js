f7612954e8473fbd99d43f3a7057b2f1fb072cc2
Fernanda-Marcelino <fernanda.marcelino2@etec.sp.gov.br>
2023-06-05 15:40:55 -0300

    arrumei a cor

--- js/script.js
+++ js/script.js
@@ -16,20 +16,15 @@ ctx.fillStyle = "black";
     ctx.stroke();
 
     //focinho
-ctx.fillStyle = "blue";
+ctx.fillStyle = "white";
 ctx.lineWidth = 3;
-    ctx.moveTo(580,200);
+    ctx.beginPath();
     ctx.lineTo(500,300);
-    ctx.moveTo(580,200);
     ctx.lineTo(588,150);
-    ctx.moveTo(587,150);
     ctx.lineTo(660,125);
-    ctx.moveTo(660,125);
     ctx.lineTo(830,160);
-    ctx.moveTo(830,160);
     ctx.lineTo(526,330);
-    ctx.moveTo(526,330);
-    ctx.lineTo(500,390);
+    ctx.lineTo(500,300);
     ctx.fill();
     ctx.stroke();
 
