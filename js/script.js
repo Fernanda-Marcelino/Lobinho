eea1de2aa6881f2227f7295d3e6db8cb1d2ea98d
Fernanda-Marcelino <fernanda.marcelino2@etec.sp.gov.br>
2023-06-05 15:21:28 -0300

    .

--- js/script.js
+++ js/script.js
@@ -5,18 +5,29 @@ const ctx = canvas.getContext ('2d');
 ctx.fillStyle = "black";
     ctx.beginPath();
     ctx.moveTo(500,300);
-    ctx.lineTo(440,300);
+    ctx.lineTo(450,300);
     ctx.moveTo(500,300);
-    ctx.lineTo(530,330);
-    ctx.lineTo(510,380);
-    ctx.lineTo(440,380);
-    ctx.lineTo(410,340);
-    ctx.lineTo(440,300);
+    ctx.lineTo(525,330);
+    ctx.lineTo(498,380);
+    ctx.lineTo(450,380);
+    ctx.lineTo(423,350);
+    ctx.lineTo(450,300);
     ctx.fill();
     ctx.stroke();
 
-    //focinhosssss
-    
+    //focinho
+ctx.fillStyle = "white";
+ctx.lineWidth = 3;
+    ctx.moveTo(580,200);
+    ctx.lineTo(500,300);
+    ctx.moveTo(580,200);
+    ctx.lineTo(588,150);
+    ctx.moveTo(587,150);
+    ctx.lineTo(660,125);
+    ctx.fill();
+    ctx.stroke();
+
+
 
     
     
