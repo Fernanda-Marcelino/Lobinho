fd759b5c8b3b6fe76ef06a8ef7efef12b99a7584
Fernanda-Marcelino <fernanda.marcelino2@etec.sp.gov.br>
2023-06-05 16:55:34 -0300

    alteração

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext ('2d');
 
 //nariz
 ctx.fillStyle = "black";
+ctx.lineWidth = 4;
     ctx.beginPath();
     ctx.lineTo(450,500);
     ctx.moveTo(500,500);
@@ -14,19 +15,44 @@ ctx.fillStyle = "black";
     ctx.fill();
     ctx.stroke();
 
-    //focinho
+//focinho lado direito
 ctx.fillStyle = "white";
 ctx.lineWidth = 4;
     ctx.beginPath();
-    ctx.lineTo(500,500);
-    ctx.lineTo(588,350);
+  
+    ctx.moveTo(500,500);
+    ctx.lineTo(560,400);
+    ctx.moveTo(560,355);
+    ctx.lineTo(560,400); 
+    ctx.moveTo(558,355);
     ctx.lineTo(660,325);
+    ctx.moveTo(660,325);
     ctx.lineTo(830,360);
+    ctx.moveTo(830,360);
     ctx.lineTo(525,530);
+    ctx.moveTo(525,530);
     ctx.lineTo(500,500);
     ctx.fill();
     ctx.stroke();
 
+//focinho lado esquerdo 
+ctx.fillStyle = "blue";
+ctx.lineWidth = 4;
+    ctx.beginPath();
+    ctx.moveTo(450,500);
+    ctx.lineTo(500,400);
+    ctx.moveTo(500,400);
+    ctx.lineTo(500,350);
+    ctx.moveTo(500,350);
+    ctx.lineTo(450,335);
+    ctx.moveTo(450,335);
+    ctx.lineTo(420,335);
+    ctx.moveTo(420,335);
+    ctx.lineTo(420,450);
+    ctx.moveTo(420,450);
+    ctx.lineTo(450,500);
+    ctx.fill();
+    ctx.stroke();
 
 
     
