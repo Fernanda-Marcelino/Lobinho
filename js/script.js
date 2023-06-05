b21d33762322974d2de7352cdf30a1abb4bebe10
Fernanda-Marcelino <fernanda.marcelino2@etec.sp.gov.br>
2023-06-05 10:27:18 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -3,15 +3,16 @@ const ctx = canvas.getContext ('2d');
 
 ctx.fillStyle = "black";
     ctx.beginPath();
-    ctx.moveTo(400,400);
-    ctx.quadraticCurveTo(200, 500,200,100);
+    ctx.moveTo(500,300);
+    ctx.lineTo(440,300);
+    ctx.moveTo(500,300);
+    ctx.lineTo(530,330);
+    ctx.lineTo(510,380);
+    ctx.lineTo(440,380);
+    ctx.lineTo(410,340);
+    ctx.lineTo(440,300);
+    ctx.moveTo
     ctx.fill();
     ctx.stroke();
-
-    ctx.fillStyle = "black";
-    ctx.beginPath();
-    ctx.moveTo(400,400);
-    ctx.quadraticCurveTo(750, 150,200,100);
-    ctx.fill();
-    ctx.stroke();
+    
 
